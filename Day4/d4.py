ba0760d7d1d634ffce2a4b754b24bc7249eb34b2
Will Durant <williamdurant@gmail.com>
2025-12-13 11:21:29 +0000

    day4 part2 solution

--- Day4/d4.py
+++ Day4/d4.py
@@ -1,7 +1,8 @@
 def identify_rolls(
     previous_row: list[str], current_row: list[str], next_row: list[str]
-) -> int:
+) -> tuple[int, str]:
     rolls_found = 0
+    converted_row = []
     for i, option in enumerate(current_row):
         start = 0 if i == 0 else i - 1
         end = i + 2
@@ -15,11 +16,15 @@ def identify_rolls(
             total_adjacent = sum(adjacency_area) - 1
             if total_adjacent < 4:
                 rolls_found += 1
+                converted_row.append("x")
+                continue
 
-    return rolls_found
+        converted_row.append(option)
+
+    return rolls_found, "".join(converted_row)
 
 
-def identify_all_rolls(rows: list[str]) -> int:
+def identify_all_rolls(rows: list[str]) -> tuple[int, str]:
     previous_row = []
     current_row = []
     next_row = []
@@ -29,16 +34,33 @@ def identify_all_rolls(rows: list[str]) -> int:
     rest_rows.append([])
 
     total_rolls_found = 0
+    converted_rows = []
     for row in rest_rows:
         next_row = [option for option in row]
 
-        rolls_found = identify_rolls(previous_row, current_row, next_row)
+        rolls_found, converted_row = identify_rolls(previous_row, current_row, next_row)
+        converted_rows.append(converted_row)
         total_rolls_found += rolls_found
 
         previous_row = current_row
         current_row = next_row
 
-    return total_rolls_found
+    return total_rolls_found, "\n".join(converted_rows)
+
+
+def identify_all_rolls_iteration(rows: list[str]) -> tuple[int, str]:
+    rows_to_iterate = rows
+    keep_iterating = True
+
+    total_rolls = 0
+    while keep_iterating:
+        total_rolls_found, test_output_str = identify_all_rolls(rows_to_iterate)
+        total_rolls += total_rolls_found
+        rows_to_iterate = test_output_str.splitlines()
+        if total_rolls_found == 0:
+            break
+
+    return total_rolls, "\n".join(rows_to_iterate)
 
 
 def main() -> None:
@@ -53,8 +75,8 @@ def main() -> None:
 .@@@@@@@@.
 @.@.@@@.@."""
     expected_output = 13
-    print(f"Part 2 Test: input:\n{test_input}\nexpected output: {expected_output}")
-    test_output = identify_all_rolls(test_input.splitlines())
+    print(f"Part 1 Test: input:\n{test_input}\nexpected output: {expected_output}")
+    test_output, test_output_str = identify_all_rolls(test_input.splitlines())
     assert test_output == expected_output, f"Test failed! Returned {test_output}"
     print("Test passed!")
 
@@ -62,9 +84,18 @@ def main() -> None:
     with open(txt_filename, "r") as f:
         rows = f.read().splitlines()
 
-    total_rolls_found = identify_all_rolls(rows)
+    total_rolls_found, test_output_str = identify_all_rolls(rows)
 
     print(f"Part 1: Total rolls found: {total_rolls_found}")
+
+    expected_output = 43
+    print(f"Part 2 Test: input:\n{test_input}\nexpected output: {expected_output}")
+    test_output, test_output_str = identify_all_rolls_iteration(test_input.splitlines())
+    assert test_output == expected_output, f"Test failed! Returned {test_output}"
+    print("Test passed!")
+
+    total_rolls_found, test_output_str = identify_all_rolls_iteration(rows)
+    print(f"Part 2: Total rolls found: {total_rolls_found}")
 
 
 if __name__ == "__main__":
